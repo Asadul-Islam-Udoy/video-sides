000fa33aac332de29c7be6b1545937eb42fee518
Asadul-Islam-Udoy <150113268+Asadul-Islam-Udoy@users.noreply.github.com>
2024-06-05 22:27:07 -0700

    Update CategoryAction.js

--- client/src/action/CategoryAction.js
+++ client/src/action/CategoryAction.js
@@ -11,7 +11,7 @@ import {
 export const getAllCategoryVideoAction = () => async (dispatch) => {
   try {
     dispatch({ type: GET_ALL_CATEGORY_REQUEST });
-    const { data } = await axios.get("/api/categories/get/all");
+    const { data } = await axios.get("https://video-sides.onrender.com/api/categories/get/all");
     dispatch({ type: GET_ALL_CATEGORY_SUCCESS, payload: data.category });
   } catch (error) {
     dispatch({
@@ -24,7 +24,7 @@ export const getAllCategoryVideoAction = () => async (dispatch) => {
 export const getSinglesCategoriesVideoAction = (id) => async (dispatch) => {
   try {
     dispatch({ type: GET_SINGLES_CATEGORIES_REQUEST });
-    const { data } = await axios.get(`/api/categories/get/single/categories/videos/${id}`);
+    const { data } = await axios.get(`https://video-sides.onrender.com/api/categories/get/single/categories/videos/${id}`);
     dispatch({ type: GET_SINGLES_CATEGORIES_SUCCESS, 
       payload: data.categories
     });
